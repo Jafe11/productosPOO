3312c2ef9f52adc90d83ed9f7c5db19331cddf13
Jafe11 <Kyukademo@gmail.com>
2023-09-25 07:51:58 -0500

    Cambios 25 sept 2023
    cambios archivos appp_inventario, se añadio la funcion registrar y empezo el selector

--- app_inventario.java
+++ app_inventario.java
@@ -5,10 +5,23 @@ public class app_inventario {
     static int int_pos = 0;
 
     public static void main(String[] args) {
-        menu(true);
+        fnt_menu(true);
     }
 
-    public static void menu (boolean m){
+    private static void fnt_registro(String nom, String desc, String codigo, int stock, float valorC, float ganan){
+        productos [int_pos] = new cls_Producto(codigo, nom, desc, stock, valorC, ganan);
+        int_pos ++;
+        JOptionPane.showMessageDialog(null, "Productos registrado",
+         "REGISTRO", JOptionPane.INFORMATION_MESSAGE);
+
+    }
+    private static void fnt_selector (int opcion){
+        if (opcion == 1){
+            
+        }
+    }
+
+    public static void fnt_menu (boolean m){
         while(m){
             int menu = Integer.parseInt(JOptionPane.showInputDialog(null, "Menu principal\n"+
             "1. Registrar\n"+
@@ -17,11 +30,7 @@ public class app_inventario {
             "4. Registro de compra\n"+
             "5. Reportes\n"+
             "6. Salir"));
-
-
-            if (menu == 6){
-                m = false;
-            }
+            fnt_selector(menu);
         }
     }
 }
